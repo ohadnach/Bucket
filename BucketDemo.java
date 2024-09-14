2f17b3a617000aab5bf3cf2fa9ce177981ff588c
ohadnach <63280001+ohadnach@users.noreply.github.com>
2024-09-14 06:54:28 +0300

    Add files via upload

---
+++ BucketDemo.java
@@ -0,0 +1,14 @@
+
+public class BucketDemo
+{
+
+	 public static void main(String[] args)
+	{
+		MyBucket b1 = new MyBucket(5);
+		MyBucket b2 = new MyBucket(3);
+		
+		System.out.println("b1 current content: " + b1.getCurrentAmount());
+		
+		
+	}
+}
